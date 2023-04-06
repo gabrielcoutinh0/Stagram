692ae6600d2166e8e099edc201a3858d2557f0c0
gabrielcoutinh0 <gabrielcoutinhof15@gmail.com>
2023-04-06 16:18:42 -0300

    feat: create register in db

--- backend/controllers/UserController.ts
+++ backend/controllers/UserController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
+const User = require("../models/User");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 const jwtSecret = process.env.JWT_SECRET;
 
@@ -9,8 +11,43 @@ const generateToken = (id: string) => {
   });
 };
 
-const register = (req: Request, res: Response) => {
-  return res.status(200).send("Registro");
+const register = async (req: Request, res: Response) => {
+  const { name, username, email, password } = req.body;
+
+  const userName = await User.findOne({ username });
+  const userMail = await User.findOne({ email });
+
+  if (userMail) {
+    res.status(422).json({ errors: ["Por favor, utilize outro e-mail"] });
+    return;
+  }
+
+  if (userName) {
+    res.status(422).json({ errors: ["Por favor, utilize outro username"] });
+    return;
+  }
+
+  const salt = await bcrypt.genSalt();
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  const newUser = await User.create({
+    name,
+    username,
+    email,
+    password: passwordHash,
+  });
+
+  if (!newUser) {
+    res
+      .status(422)
+      .json({ erros: ["Houve um erro, por favor tente mais tarde."] });
+    return;
+  }
+
+  res.status(201).json({
+    _id: newUser._id,
+    token: generateToken(newUser._id),
+  });
 };
 
 module.exports = { register };
