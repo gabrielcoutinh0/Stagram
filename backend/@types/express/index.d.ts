6ef1c9d1e15f2fbe08c3cd2a9d36bea3449e41b4
gabrielcoutinh0 <gabrielcoutinhof15@gmail.com>
2023-04-10 18:24:16 -0300

    fix: fixing user typing in validation

--- backend/@types/express/index.d.ts
+++ backend/@types/express/index.d.ts
@@ -1,13 +1,9 @@
 declare namespace Express {
   interface Request {
     user: {
-      id: string;
       name: string;
       username: string;
       email: string;
-      password: string;
-      profileImage?: string;
-      bio?: string;
     };
   }
 }
