909d78447a40a4fd4de0b6ee87e28e78a165eafa
gabrielcoutinh0 <gabrielcoutinhof15@gmail.com>
2023-04-18 12:08:38 -0300

    feat: create navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,11 +3,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home/Home";
 import { Login } from "./pages/Auth/Login";
 import { Register } from "./pages/Auth/Register";
+import { Navbar } from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
---
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,63 @@
+import styles from "./Navbar.module.css";
+import { Link } from "react-router-dom";
+import { BsGear } from "react-icons/bs";
+import { AiOutlineHome, AiOutlineSearch } from "react-icons/ai";
+import { VscDiffAdded } from "react-icons/vsc";
+import { useEffect, useState } from "react";
+import { IconContext } from "react-icons/lib";
+
+export function Navbar() {
+  const [theme, setTheme] = useState("dark");
+
+  useEffect(() => {
+    document.documentElement.setAttribute("color-scheme", theme);
+  }, [theme]);
+
+  return (
+    <header>
+      <div className={styles.container}>
+        <div className={styles.logo}>
+          <i aria-label="Stagram" role="img" />
+        </div>
+        <div className={styles.search}>
+          <form>
+            <input type="text" placeholder="Pesquisar" />
+          </form>
+        </div>
+        <nav>
+          <IconContext.Provider value={{ className: "icon", size: "1.5rem" }}>
+            <div className={styles.tooltip}>
+              <Link to="/">
+                <AiOutlineHome />
+                <span className={styles.tooltiptext}>Home</span>
+              </Link>
+            </div>
+            <div className={styles.tooltip}>
+              <Link to="/">
+                <VscDiffAdded />
+                <span className={styles.tooltiptext}>Add Photo</span>
+              </Link>
+            </div>
+            <div className={styles.tooltip}>
+              <button
+                aria-label={`Change theme to ${
+                  theme === "light" ? "dark" : "light"
+                } mode`}
+                role="switch"
+                onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+              >
+                <BsGear />
+                <span className={styles.tooltiptext}>Settings</span>
+              </button>
+            </div>
+            <div className={styles.tooltip}>
+              <div className={styles.perfil}></div>
+              <span className={styles.tooltiptext}>Perfil</span>
+            </div>
+          </IconContext.Provider>
+        </nav>
+      </div>
+      <div className={styles.separator} />
+    </header>
+  );
+}
