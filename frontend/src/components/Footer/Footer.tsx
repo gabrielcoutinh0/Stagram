2be78c4536ace548a8862b535d1b7bf79a3ff572
gabrielcoutinh0 <gabrielcoutinhof15@gmail.com>
2023-04-25 10:28:36 -0300

    feat: remove footer if user is logged in

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,12 +1,18 @@
+import { useAuth } from "../../hooks/useAuth";
 import styles from "./Footer.module.css";
 
 export function Footer() {
+  const { auth } = useAuth();
   return (
-    <footer>
-      <div className={styles.separator} />
-      <div className={styles.footer}>
-        &copy; {new Date().getFullYear()} Stagram
-      </div>
-    </footer>
+    <>
+      {!auth && (
+        <footer>
+          <div className={styles.separator} />
+          <div className={styles.footer}>
+            &copy; {new Date().getFullYear()} Stagram
+          </div>
+        </footer>
+      )}
+    </>
   );
 }
