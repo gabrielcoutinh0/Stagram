7946233cb463c49289461c5e5705a213a690d67d
gabrielcoutinh0 <gabrielcoutinhof15@gmail.com>
2023-04-25 08:37:45 -0300

    fix: redoing the store

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,16 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+
 import authReducer from "./slices/authSlice";
-import { setupListeners } from "@reduxjs/toolkit/query";
-import { authApi } from "./api/authApi";
+
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    [authApi.reducerPath]: authApi.reducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(authApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-setupListeners(store.dispatch);
