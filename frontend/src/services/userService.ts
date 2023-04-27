891805d979b9bfe34368347af5062076853a5da4
gabrielcoutinh0 <gabrielcoutinhof15@gmail.com>
2023-04-27 10:11:36 -0300

    feat: fetch api profile

---
+++ frontend/src/services/userService.ts
@@ -0,0 +1,22 @@
+import { api, requestConfig } from "../utils/config";
+import { IData } from "../utils/type";
+
+async function profile(data: IData, token: string) {
+  const config = requestConfig("GET", data, token);
+
+  try {
+    const res = await fetch(api + "/users/profile", config)
+      .then((res) => res.json())
+      .catch((err) => err);
+
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const userService = {
+  profile,
+};
+
+export default userService;
