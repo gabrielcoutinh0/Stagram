d6fd493f831416dd9185fefd5eaf559c89924868
gabrielcoutinh0 <gabrielcoutinhof15@gmail.com>
2023-04-27 10:12:14 -0300

    style: add photo user in navbar

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,17 +1,29 @@
 import styles from "./Navbar.module.css";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { AiOutlineHome } from "react-icons/ai";
 import { VscDiffAdded } from "react-icons/vsc";
 import { IconContext } from "react-icons/lib";
 import { useAuth } from "../../hooks/useRequireAuth";
 import { useSelector } from "react-redux";
-import { RootState } from "../../store";
+import { AppDispatch, RootState } from "../../store";
 import { Dropdown } from "../Dropdown/Dropdown";
 import { themeType } from "../../utils/type";
+import { useDispatch } from "react-redux";
+import { profile } from "../../slices/userSlice";
+import { uploads } from "../../utils/config";
 
 export function Navbar({ theme, setTheme }: themeType) {
+  const dispatch = useDispatch<AppDispatch>();
+  const { user, message, error, loading } = useSelector(
+    (state: RootState) => state.user
+  );
+
   const { auth } = useAuth();
-  const { user } = useSelector((state: RootState) => state.auth);
+
+  useEffect(() => {
+    dispatch(profile());
+  }, [dispatch]);
 
   return (
     <>
@@ -45,24 +57,36 @@ export function Navbar({ theme, setTheme }: themeType) {
               <IconContext.Provider
                 value={{ className: "icon", size: "1.5rem" }}
               >
-                <div className={styles.tooltip}>
-                  <Link to="/">
+                <Link to="/" className={styles.tooltip}>
+                  <div>
                     <AiOutlineHome />
                     <span className={styles.tooltiptext}>Home</span>
-                  </Link>
-                </div>
-                <div className={styles.tooltip}>
-                  <Link to="/">
+                  </div>
+                </Link>
+                <Link to="/" className={styles.tooltip}>
+                  <div>
                     <VscDiffAdded />
                     <span className={styles.tooltiptext}>Add Photo</span>
-                  </Link>
-                </div>
-                <div className={styles.tooltip}>
-                  <Link to="/profile">
-                    <div className={styles.perfil}></div>
+                  </div>
+                </Link>
+                <Link to="/profile">
+                  <div className={styles.tooltip}>
+                    <div className={styles.perfil}>
+                      {user?.profileImage ? (
+                        <img
+                          src={`${uploads}/users/${user?.profileImage}`}
+                          alt={`Foto de ${user?.name}`}
+                        />
+                      ) : (
+                        <img
+                          src="./userWithoutPhoto.jpg"
+                          alt="Usuário sem foto"
+                        />
+                      )}
+                    </div>
                     <span className={styles.tooltiptext}>Perfil</span>
-                  </Link>
-                </div>
+                  </div>
+                </Link>
               </IconContext.Provider>
               <Dropdown theme={theme} setTheme={setTheme} />
             </nav>
