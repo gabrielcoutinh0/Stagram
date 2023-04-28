95f5d43350b1671a4d44300353cf34ecf12f2981
gabrielcoutinh0 <gabrielcoutinhof15@gmail.com>
2023-04-28 09:35:49 -0300

    refactor: replacing if and else with ternary operator

--- frontend/src/utils/config.ts
+++ frontend/src/utils/config.ts
@@ -1,40 +1,23 @@
-import { IRegister, ILogin } from "./type";
+import { IData } from "./type";
 
 export const api = import.meta.env.VITE_SERVER_ENDPOINT;
 export const uploads = import.meta.env.VITE_UPLOADS_SERVER;
 
 export function requestConfig(
   method: string,
-  data: IRegister | ILogin | string,
-  token: string | null = null,
-  image: boolean | null = null
+  data?: IData,
+  token: string | null = null
 ) {
   let config: RequestInit;
 
-  if (image) {
-    config = {
-      method,
-      body: JSON.stringify(data),
-      headers: {},
-    };
-  } else if (method === "DELETE" || data === null) {
-    config = {
-      method,
-      headers: {},
-    };
-  } else {
-    config = {
-      method,
-      body: JSON.stringify(data),
-      headers: { "Content-Type": "application/json" },
-    };
-  }
-
-  if (token) {
-    config = {
-      headers: { Authorization: `Bearer ${token}` },
-    };
-  }
+  config = {
+    method,
+    body: method === "PUT" ? (data as BodyInit) : JSON.stringify(data) || null,
+    headers:
+      token || method === "DELETE" || data === null
+        ? { Authorization: `Bearer ${token}` }
+        : { "Content-Type": "application/json" },
+  };
 
   return config;
 }
