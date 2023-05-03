1269ad22983e55eb31b1124f143ca1c7bf50be4a
gabrielcoutinh0 <gabrielcoutinhof15@gmail.com>
2023-05-02 22:14:58 -0300

    feat: shows if there is no profile with that username

--- frontend/src/pages/Profile/Profile.tsx
+++ frontend/src/pages/Profile/Profile.tsx
@@ -7,12 +7,15 @@ import { useSelector } from "react-redux";
 import { getUserDetails } from "../../slices/userSlice";
 import { uploads } from "../../utils/config";
 import { Link } from "react-router-dom";
+import { IData } from "../../utils/type";
 
 export function Profile() {
   const { id } = useParams();
   const dispatch = useDispatch<AppDispatch>();
 
-  const { user, loading } = useSelector((state: RootState) => state.user);
+  const { user, loading, error } = useSelector(
+    (state: RootState) => state.user
+  );
   const { user: userAuth } = useSelector((state: RootState) => state.auth);
 
   useEffect(() => {
@@ -21,57 +24,65 @@ export function Profile() {
 
   if (loading) return <p>Carregando...</p>;
 
-  console.log(user);
-  console.log(userAuth);
-  //{user?._id === userAuth?._id && <button>editar</button>}
-
   return (
     <div className={styles.container}>
-      <header className={styles.headerProfile}>
-        <div className={styles.wrapperPicture}>
-          <div className={styles.picture}>
-            {user?.profileImage ? (
-              <img
-                src={`${uploads}/users/${user?.profileImage}`}
-                alt={`Foto de ${user?.name}`}
-              />
-            ) : (
-              <img src="/userWithoutPhoto.jpg" alt="Usuário sem foto" />
-            )}
-          </div>
+      {error ? (
+        <div className={styles.notProfile}>
+          <span>Esta página não está disponível.</span>
+          <p>
+            O link em que você clicou pode não estar funcionando, ou a página
+            pode ter sido removida. <Link to="/">Voltar para o Instagram.</Link>
+          </p>
         </div>
-        <section className={styles.dataProfile}>
-          <div className={styles.wrapperUsernameAndEdit}>
-            <h2>{user?.username}</h2>
-            {user?._id === userAuth?._id && (
-              <Link to="/profile/edit">
-                <div className={styles.btnEdit}>Editar perfil</div>
-              </Link>
-            )}
-          </div>
-          <div className={styles.publications}>
+      ) : (
+        <>
+          <header className={styles.headerProfile}>
+            <div className={styles.wrapperPicture}>
+              <div className={styles.picture}>
+                {user?.profileImage ? (
+                  <img
+                    src={`${uploads}/users/${user?.profileImage}`}
+                    alt={`Foto de ${user?.name}`}
+                  />
+                ) : (
+                  <img src="/userWithoutPhoto.jpg" alt="Usuário sem foto" />
+                )}
+              </div>
+            </div>
+            <section className={styles.dataProfile}>
+              <div className={styles.wrapperUsernameAndEdit}>
+                <h2>{user?.username}</h2>
+                {user?._id === userAuth?._id && (
+                  <Link to="/profile/edit">
+                    <div className={styles.btnEdit}>Editar perfil</div>
+                  </Link>
+                )}
+              </div>
+              <div className={styles.publications}>
+                <span>{user?.photosPosted?.length} </span>
+                publicações
+              </div>
+              <div className={styles.nameAndBio}>
+                <span className={styles.nameProfile}>{user?.name}</span>
+                <div className={styles.bio}>
+                  <span>{user?.bio}</span>
+                </div>
+              </div>
+            </section>
+          </header>
+          <div className={styles.publicationsMobile}>
             <span>{user?.photosPosted?.length} </span>
             publicações
           </div>
-          <div className={styles.nameAndBio}>
+          <div className={styles.nameAndBioMobile}>
             <span className={styles.nameProfile}>{user?.name}</span>
             <div className={styles.bio}>
               <span>{user?.bio}</span>
             </div>
           </div>
-        </section>
-      </header>
-      <div className={styles.publicationsMobile}>
-        <span>{user?.photosPosted?.length} </span>
-        publicações
-      </div>
-      <div className={styles.nameAndBioMobile}>
-        <span className={styles.nameProfile}>{user?.name}</span>
-        <div className={styles.bio}>
-          <span>{user?.bio}</span>
-        </div>
-      </div>
-      <div className={styles.separator} />
+          <div className={styles.separator} />
+        </>
+      )}
     </div>
   );
 }
